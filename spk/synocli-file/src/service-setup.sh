3a8309b2dde3d215f1bcb866b83fd91eec94906b
m4tt075 <mgawalter@web.de>
2019-01-27 23:24:12 +0100

    [SynoCLI-file] Add renamer 1.10 to package

--- spk/synocli-file/src/service-setup.sh
+++ spk/synocli-file/src/service-setup.sh
@@ -1,6 +1,5 @@
-
 COMMANDS="jdupes less lessecho lesskey tree ed2k-link edonr256-hash edonr512-hash ghost-hash has160-hash magnet-link"
-COMMANDS+="rhash sfv-hash tiger-hash tth-hash whirlpool-hash"
+COMMANDS+="rhash sfv-hash tiger-hash tth-hash whirlpool-hash rename"
 
 service_postinst ()
 {
@@ -10,6 +9,11 @@ service_postinst ()
             ln -s "${SYNOPKG_PKGDEST}/bin/$cmd" "/usr/local/bin/$cmd"
         fi
     done
+
+    # rename requires perl modules to be linked into local installation
+    ln -s "${SYNOPKG_PKGDEST}/share/perl5/File/Rename.pm" "/usr/local/share/perl5/vendor_perl/File/Rename.pm"
+    mkdir "/usr/local/share/perl5/vendor_perl/File/Rename"
+    ln -s "${SYNOPKG_PKGDEST}/share/perl5/File/Rename/Options.pm" "/usr/local/share/perl5/vendor_perl/File/Rename/Options.pm"
 }
 
 service_postuninst ()
@@ -20,4 +24,12 @@ service_postuninst ()
             rm -f "/usr/local/bin/$cmd"
         fi
     done
+    # Remove rename's perl modules from the local installation
+    if [ -e "/usr/local/share/perl5/vendor_perl/File/Rename.pm" ]; then
+        rm -f "/usr/local/share/perl5/vendor_perl/File/Rename.pm"
+    fi
+    if [ -e "/usr/local/share/perl5/vendor_perl/File/Rename/Options.pm" ]; then
+        rm -f "/usr/local/share/perl5/vendor_perl/File/Rename/Options.pm"
+        rmdir "/usr/local/share/perl5/vendor_perl/File/Rename"
+    fi
 }
